6dbc2eb2660892b4e8b01f1d13e8c333ef5edbf1
alexxsanya <alexxsanya@gmail.com>
2019-03-19 16:12:21 +0300

    [#164728068] Removed the js code which generated the date & time

--- UI/static/js/App.js
+++ UI/static/js/App.js
@@ -12,19 +12,6 @@ loadLocalHTML = function (uri){
     xmlhttp.send();
 }
 
-setLiveTime = function(){
-    var refresh=1000; 
-    mytime=setTimeout('setCurrentTime()',refresh)
-}
-setCurrentTime = function () {
-    var today = new Date();
-    var date = today.getDate()+'-'+ (today.getMonth()+1)+'-'+today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
-    setLiveTime()
-    document.getElementById('time-date').innerHTML = dateTime;
-};
-
 loadMessage = function(caption){
 
     document.getElementById('main-body').innerHTML = "Loading...";
